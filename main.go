4ee1eef45d2922451c370bdabb377449c71d3835
uni51 <unieye51@gmail.com>
2024-06-23 12:02:32 +0900

    3.4 NULLかもしれない値を受け取る3.4 NULLかもしれない値を受け取る3.4 NULLかもしれない値を受け取る3.4 NULLかもしれない値を受け取る

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"dbsample/models"
 	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -19,9 +20,36 @@ func main() {
 	}
 	defer db.Close()
 
-	if err := db.Ping(); err != nil {
+	const sqlStr = `
+		select * from articles;
+	`
+	rows, err := db.Query(sqlStr)
+	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println("connect to DB")
+		return
 	}
+	defer rows.Close()
+
+	// / models.Article 型のスライスを初期化します。これはデータベースから取得した記事を格納するために使用します。
+	articleArray := make([]models.Article, 0) // スライスの初期長さは0です。つまり、スライスは空（要素を持たない）状態で作成されます。
+	// データベースのクエリ結果を行単位で処理します。
+	for rows.Next() {
+		var article models.Article
+		var createdTime sql.NullTime
+		// rows.Scan で各列のデータを article 変数に読み込みます。
+		err := rows.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
+
+		if createdTime.Valid {
+			article.CreatedAt = createdTime.Time
+		}
+
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			// エラーがなければ、article 変数を articleArray スライスに追加します。
+			articleArray = append(articleArray, article)
+		}
+	}
+
+	fmt.Printf("%+v\n", articleArray)
 }
